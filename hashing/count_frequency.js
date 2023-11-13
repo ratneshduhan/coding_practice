7597897c2752c6e476558b1c88ae750cfea58767
Ratnesh Duhan <duhan.ratnesh@gmail.com>
2023-11-13 16:28:57 +0530

    alternate solution for count_frequency

--- hashing/count_frequency.js
+++ hashing/count_frequency.js
@@ -1,14 +1,36 @@
 // count frequency of elements in an array
 
-function count_frequency(array) {
+function count_frequency_optimal(array) {
     let map = new Map();
+
     for (let i of array) {
         if (map.has(i)) {
             map.set(i, map.get(i) + 1);
-        } else map.set(i, 1);
+        } else
+            map.set(i, 1);
     }
+
     map.forEach((value, key) => console.log(`key: ${key}, freq: ${value}`));
 }
 
+function count_frequency(array) {
+    let visited = new Array(array.length).fill(false);
+    for (let i = 0; i < array.length; i++) {
+
+        if (visited[i])
+            continue;
+
+        let count = 1;
+        for (let j = i + 1; j <= array.length; j++) {
+            if (array[i] == array[j]) {
+                visited[j] = true;
+                count++;
+            }
+        }
+        console.log(`Freq of ${array[i]}: ${count}`);
+    }
+}
+
 let array = [1, 2, 3, 4, 5, 3, 7, 8, 4, 7, 1, 4, 2, 4, 9];
-console.log(`Frequencies of elements: ${count_frequency(array)}`);
+count_frequency_optimal(array);
+count_frequency(array);
